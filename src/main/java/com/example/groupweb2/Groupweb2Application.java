db98681ba33e7b7f4fa04f7990a8b163d262b2fc
Luukien61 <kienluu61@gmail.com>
2024-05-14 19:44:28 +0700

    initial commit

---
+++ src/main/java/com/example/groupweb2/Groupweb2Application.java
@@ -0,0 +1,13 @@
+package com.example.groupweb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Groupweb2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Groupweb2Application.class, args);
+    }
+
+}
